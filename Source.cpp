4910d5cc217499ecf9b7becec3ab1b540dd2950c
Thinh Pham <kyle.pham202@gmail.com>
2024-04-20 19:32:25 -0400

    glfw, window initialization and configuration

--- Source.cpp
+++ Source.cpp
@@ -2,19 +2,66 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main() {
-	
+	// glfw initialization and configuration
+	// -------------------------------------
 	if (!glfwInit()) {
 		printf("Failed initialization");
 		return -1;
 	}
+
+	// window initialization and configuration
+	// --------------------------------------
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "DisplayThings", NULL, NULL);
+	if (window == NULL) {
+		printf("Error: Failed to create window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		printf("Error: Failed to initialize GLAD");
+		return -1;
+	}
+
 	
-	scanf_s("waiting for input: ");
+	// declare viewport dimensions
+	glViewport(0, 0, 800, 600);
+	
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		
+		// input
+		processInput(window);
+
+		// rendering commands
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 	glfwTerminate();
 	return 0;
 }
 
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
