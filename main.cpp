226136cb0c4472da1ed477110d8e3fe62945adbc
Ternt <kyle.pham202@gmail.com>
2024-05-15 08:41:32 -0400

    deleted src file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// --------------------------- TODOs ---------------------------
+// finish cmake build so that it include glad and glfw.
+
+int main(){
+	printf("hello world!");
+  return 0;
+}
+
+
+
+
