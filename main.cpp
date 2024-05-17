fdfc1385cc79f5091514400c1dd599e2e5b41ad8
Ternt <kyle.pham202@gmail.com>
2024-05-17 12:43:58 -0400

    still doing cmake

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 // finish cmake build so that it include glad and glfw.
 
 int main(){
-	printf("hello world!");
+  int letter = 0;
+  printf("hello world!");
+  scanf("type anything to exit %d", &letter);
   return 0;
 }
 
