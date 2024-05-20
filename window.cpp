0ad07b1b877582eb60e3ef650aa8a3bb588cab18
Ternt <kyle.pham202@gmail.com>
2024-05-20 14:09:50 -0400

    include path

--- window.cpp
+++ window.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "glad/glad.h" 
-#include "GLFW/glfw3.h" 
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h> 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
