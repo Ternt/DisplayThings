eba4adafc4adb2818bfeb95089790fcab0436dbf
Ternt <kyle.pham202@gmail.com>
2024-05-23 14:04:51 -0400

    shader loader

--- window.cpp
+++ window.cpp
@@ -9,7 +9,8 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void AddShader(const char* shaderText);
+void LoadShaderSource(const char* shaderText, GLuint shader);
+void createShaderProgram();
 
 const unsigned int width = 800; 
 const unsigned int height = 600;
@@ -47,13 +48,7 @@ int main() {
 	}
 
 
-  std::string vertexSource;
-  const char* pVSFileName = "src/vert.glsl";
-  ReadFile(pVSFileName, vertexSource);
-  
-  // create a shader object
-  // ----------------------
-  AddShader(vertexSource.c_str());
+  createShaderProgram();
 
 	// render loop
   // -----------
@@ -96,8 +91,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 
 // shader compilation: deals with the creation, compilation, and linking of shader object files to a shader program
 // ----------------------------------------------------------------------------------------------------------------
-void AddShader(const char* shaderText){
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+void LoadShaderSource(const char* shaderText, GLuint shaderObject){
   
   const GLchar* p[1];
   p[0] = shaderText;
@@ -105,28 +99,107 @@ void AddShader(const char* shaderText){
   GLint Lengths[1];
   Lengths[0] = (GLint)strlen(shaderText);
 
-  glShaderSource(vertexShader, 1, p, Lengths);
+  glShaderSource(shaderObject, 1, p, Lengths);
+}
 
-  // shader compilation and error handling 
-  // -------------------------------------
-  glCompileShader(vertexShader);
+
+// shader compilation and error handling 
+// -------------------------------------
+const char* pVSFileName = "src/basic.vert";
+const char* pFSFileName = "src/basic.frag";
+void createShaderProgram(){
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   GLint isCompiled = 0;
+
+
+  std::string vertexSource, fragmentSource;
+  if(!ReadFile(pVSFileName, vertexSource)){
+    std::cout << "File was not read.\n";
+    exit(1);
+  }
+
+  if(!ReadFile(pVSFileName, vertexSource)){
+    std::cout << "File was not read.\n";
+    exit(1);
+  }
+
+
+  LoadShaderSource(vertexSource.c_str(), vertexShader);
+  glCompileShader(vertexShader);
+
   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
   if(isCompiled == GL_FALSE)
   {
     GLint maxLength = 0;
     glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
 
+    std::cout << "Vertex shader compiled unsuccessfully.\n";
+
     // The maxLength includes the NULL character
     std::vector<GLchar> infoLog(maxLength);
     glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
 
-    // We don't need the shader anymore.
     glDeleteShader(vertexShader);
 
-    // Use the infoLog as you see fit.
-
-    // In this simple program, we'll just leave
     exit(1);
   }
+
+
+  LoadShaderSource(fragmentSource.c_str(), fragmentShader);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+  if(isCompiled == GL_FALSE)
+  {
+    GLint maxLength = 0;
+    glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+    std::cout << "Fragment shader compiled unsuccessfully.\n";
+
+    // The maxLength includes the NULL character
+    std::vector<GLchar> infoLog(maxLength);
+    glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+
+    glDeleteShader(fragmentShader);
+
+    exit(1);
+  }
+
+
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+
+  glLinkProgram(program);
+
+  GLint isLinked = 0;
+  glGetProgramiv(program, GL_LINK_STATUS, (int *)&isLinked);
+  if (isLinked == GL_FALSE)
+  {
+    GLint maxLength = 0;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+    // The maxLength includes the NULL character
+    std::vector<GLchar> infoLog(maxLength);
+    glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+
+    std::cout << "Shaders linked unsuccessfully.\n";
+
+    // We don't need the program anymore.
+    glDeleteProgram(program);
+    // Don't leak shaders either.
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    exit(1); 
+  }
+
+  // Always detach shaders after a successful link.
+  glDetachShader(program, vertexShader);
+  glDetachShader(program, fragmentShader);
+
+  glUseProgram(program);
 }
